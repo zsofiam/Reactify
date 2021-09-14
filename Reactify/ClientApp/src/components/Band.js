4ce398884bed63851180bd0957e33307e8c1bf46
csilla-hegedus <hegeduscsilla333@gmail.com>
2021-09-14 18:49:33 +0200

    [show] dummy object data in search-band route using React components

---
+++ Reactify/ClientApp/src/components/Band.js
@@ -0,0 +1,16 @@
+﻿import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Band = (props) => {
+
+    return (
+        <div>
+            <p>{props.bandData.name}</p>
+            <p>{props.bandData.genre}</p>
+            <p>{props.bandData.country}</p>
+            <p>{props.bandData.biography}</p>
+        </div>
+    )
+}
+
+export default Band;
---
+++ Reactify/ClientApp/src/components/BandDetail.js
@@ -0,0 +1,22 @@
+﻿import React, { useState } from 'react';
+
+import Band from './Band.js';
+
+const BandDetail = () => {
+    const [searchedBand, setSearchedBand] = useState({
+        "id": "1",
+        "name": "Linkin Park",
+        "genre": "Alternative Rock",
+        "country": "California, USA",
+        "website": "www.linkinpark.com",
+        "biography": "Linkin Park is an American rock band from Agoura Hills, California, formed in 1996.",
+    });
+
+    return (
+        <div>
+            <Band bandData={searchedBand} />
+        </div>
+    )
+}
+
+export default BandDetail;
