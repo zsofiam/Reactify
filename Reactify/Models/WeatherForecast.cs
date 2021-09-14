e56a74ec3dfbe03455b4974fde8fc78a0d1baf8e
MoniBohm <monic.bohm@gmail.com>
2021-09-14 16:09:28 +0200

    [add] MusicPlyer to Modal

--- Reactify/Models/WeatherForecast.cs
+++ Reactify/Models/WeatherForecast.cs
@@ -2,14 +2,14 @@ using System;
 
 namespace Reactify
 {
-    public class WeatherForecast
+    public class MusicPlayer
     {
-        public DateTime Date { get; set; }
+        //public DateTime Date { get; set; }
 
-        public int TemperatureC { get; set; }
+        //public int TemperatureC { get; set; }
 
-        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+        //public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 
-        public string Summary { get; set; }
+        //public string Summary { get; set; }
     }
 }
