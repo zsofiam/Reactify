d348d3b000f1a09f0c8566e1ca8d1a571120332d
Zsofia Matyasi <zsofia.matyasi@gmail.com>
2021-09-14 13:27:39 +0200

    feat: add Event class to Models

---
+++ Reactify/Models/Event.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Reactify.Models
+{
+    public class Event
+    {
+        public long Id { get; set; }
+        public string Title { get; set; }
+        public string Band { get; set; }
+        public DateTime Date { get; set; }
+        public string Venue { get; set; }
+    }
+}
