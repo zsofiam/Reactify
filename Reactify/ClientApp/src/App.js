813700c8d35dcf18276784cb736ccbf4bfeed518
MoniBohm <monic.bohm@gmail.com>
2021-09-14 13:42:31 +0200

    [fix] hook format

--- Reactify/ClientApp/src/App.js
+++ Reactify/ClientApp/src/App.js
@@ -1,20 +1,19 @@
 import React, { Component } from 'react';
-import { Route } from 'react-router';
-import { Layout } from './components/Layout';
-import { Home } from './components/Home';
-import { FetchData } from './components/FetchData';
-import { Counter } from './components/Counter';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Home from './components/Home';
 
 import './custom.css'
 
-function App(){
+function App() {
 
     return (
-      <Layout>
-        <Route exact path='/' component={Home} />
-        <Route path='/counter' component={Counter} />
-        <Route path='/fetch-data' component={FetchData} />
-      </Layout>
+        <Router>
+            <Switch>
+                <Route path="/" exact >
+                    <Home />
+                </Route>
+            </Switch>
+        </Router>
     );
 }
 
