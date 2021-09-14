853865f7ca00668e2ddc35eb7acc6f9abdc5d1f3
BrizaMD <late16@gmail.com>
2021-09-14 13:10:48 +0200

    [refactor] Home to hook component

--- Reactify/ClientApp/src/components/Home.js
+++ Reactify/ClientApp/src/components/Home.js
@@ -1,9 +1,7 @@
 import React, { Component } from 'react';
 
-export class Home extends Component {
-  static displayName = Home.name;
-
-  render () {
+export const Home = () => {
+  
     return (
       <div>
         <h1>Hello, world!</h1>
@@ -22,5 +20,5 @@ export class Home extends Component {
         <p>The <code>ClientApp</code> subdirectory is a standard React application based on the <code>create-react-app</code> template. If you open a command prompt in that directory, you can run <code>npm</code> commands such as <code>npm test</code> or <code>npm install</code>.</p>
       </div>
     );
-  }
+  
 }
