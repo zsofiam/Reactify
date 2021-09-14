cb255c055632c9ab2c1487569606e6f7d0e10b0f
MoniBohm <monic.bohm@gmail.com>
2021-09-14 19:25:21 +0200

    [add] fontawesome icons to social media section

--- Reactify/ClientApp/src/App.js
+++ Reactify/ClientApp/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from './components/Home';
 import MusicPlayer from './components/musicPlayer/MusicPlayer';
+import Footer from './components/footer/Footer.js';
 
 import './custom.css'
 
@@ -18,6 +19,7 @@ function App() {
                 </Route>
 
             </Switch>
+            <Footer />
         </Router>
     );
 }
--- Reactify/ClientApp/src/components/footer/Footer.js
+++ Reactify/ClientApp/src/components/footer/Footer.js
@@ -1,13 +1,40 @@
 import React, { Component, useState } from 'react';
+import './Footer.css';
 
-const PlayerControls = () => {
+const Footer = () => {
 
     return (
-        <div>
-            <p>Ez a player</p>
-        </div>
+        <>
+            <div class="footer">
+                <div class="contain">
+                    <div class="col">
+                        <h1>Support</h1>
+                        <ul>
+                            <li>Contact us</li>
+                            <li>Web chat</li>
+                            <li>Open ticket</li>
+                        </ul>
+                    </div>
+                    <div class="col social">
+                        <h1>Social</h1>
+                        <ul>
+                            <li>
+                                <i className='fab fa-facebook-f' />
+                            </li>
+                            <li>
+                                <i className='fab fa-instagram' />
+                            </li>
+                            <li>
+                                <i className='fab fa-youtube' />
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="clearfix"></div>
+                </div>
+            </div>
+        </>
     );
 
 }
 
-export default PlayerControls;
+export default Footer;
