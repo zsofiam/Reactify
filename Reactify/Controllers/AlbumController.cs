77d0b28751f990ef64631e49c6521df8c9094bbb
MoniBohm <monic.bohm@gmail.com>
2021-09-15 22:06:51 +0200

    [implement] backend for album data

---
+++ Reactify/Controllers/AlbumController.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json.Linq;
+using Reactify.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace Reactify.Controllers
+{
+    [ApiController]
+    [Route("player")]
+
+    public class AlbumController : ControllerBase
+    {
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public AlbumController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public async Task<List<Album>> GetResultTracks([FromQuery] string albumId)  // it was type IEnumerable<Track> void or Task type?
+        {
+
+
+            string url = "https://api.deezer.com/album/" + albumId.Trim().Replace(" ", "_");
+            using (var httpClient = new HttpClient())
+            {
+                httpClient.DefaultRequestHeaders.Add("User-Agent", "Anything");
+                httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                var response = httpClient.GetAsync(url).Result;
+                response.EnsureSuccessStatusCode();
+                string apiResponse = await response.Content.ReadAsStringAsync();
+                var albumData = JObject.Parse(apiResponse);
+                var result = new List<Album>();
+
+
+                Album album = new Album();
+                album.Title = (string)albumData["title"];
+                album.Cover = (string)albumData["cover_xl"];
+                album.Artist = (string)albumData["artist"]["name"];
+                List<Track> tracks = new List<Track>();
+
+                foreach (var track in albumData["tracks"]["data"])
+                {
+                    Track song = new Track();
+                    song.Preview = (string)track["preview"];
+                    song.Title = (string)track["title"];
+                    tracks.Add(song);
+                }
+                album.Tracks = tracks;
+                result.Add(album);
+                return result;
+
+            }
+
+        }
+    }
+}
--- Reactify/Controllers/MusicPlayerController.cs
+++ Reactify/Controllers/MusicPlayerController.cs
@@ -12,6 +12,7 @@ namespace Reactify.Controllers
 {
     [ApiController]
     [Route("track")]
+
     public class MusicPlayerController : ControllerBase
     {
         private readonly ILogger<WeatherForecastController> _logger;
@@ -74,18 +75,18 @@ namespace Reactify.Controllers
         }
 
         //get specific track
-        [HttpGet("{id}")]
-        public IEnumerable<Track> GetTrack()
-        {
-            return Enumerable.Range(1, 5).Select(index => new Track
-            {
-                Id = "1",
-                Duration = "60",
-                ReleaseDate = "tegnap",
-                Title = "Fake Title",
-                Preview = "https://cdns-preview-d.dzcdn.net/stream/c-deda7fa9316d9e9e880d2c6207e92260-8.mp3"
-            })
-            .ToArray();
-        }
+        //[HttpGet]
+        //public IEnumerable<Track> GetTrack()
+        //{
+        //    return Enumerable.Range(1, 5).Select(index => new Track
+        //    {
+        //        Id = "1",
+        //        Duration = "60",
+        //        ReleaseDate = "tegnap",
+        //        Title = "Fake Title",
+        //        Preview = "https://cdns-preview-d.dzcdn.net/stream/c-deda7fa9316d9e9e880d2c6207e92260-8.mp3"
+        //    })
+        //    .ToArray();
+        //}
     }
 }
