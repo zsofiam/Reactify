63393844339e008ff256a9ca6dd2da959cfa6d89
BrizaMD <late16@gmail.com>
2021-09-15 10:08:01 +0200

    [add] Album class

---
+++ Reactify/Models/Album.cs
@@ -0,0 +1,10 @@
+﻿namespace Reactify.Models
+{
+    public class Album
+    {
+        public string Id { get; set; }
+        public string Title { get; set; }
+        public string Cover { get; set; }
+        public string ReleaseDate { get; set; }
+    }
+}
