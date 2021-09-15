333c2cdde2c0e67464dd5b9721a788dfc9e463ea
csilla-hegedus <hegeduscsilla333@gmail.com>
2021-09-15 20:16:23 +0200

    [add] classes to band detail page

--- Reactify/ClientApp/src/components/Band.js
+++ Reactify/ClientApp/src/components/Band.js
@@ -3,12 +3,12 @@
 const Band = (props) => {
 
     return (
-        <div>
-            <p>{props.bandData.name}</p>
-            <img src={props.bandData.image} alt="picture about the band logo"/>
-            <p>{props.bandData.genre}</p>
-            <p>{props.bandData.country}</p>
-            <p>{props.bandData.biography}</p>
+        <div className="band-details">
+            <img classname="band-banner" src={props.bandData.image} alt="picture about the band logo" />
+            <p className="band-info">Genre: {props.bandData.genre}</p>
+            <p className="band-info">Country: {props.bandData.country}</p>
+            <p className="band-info">Biograpy:</p>
+            <p className="band-info biography">{props.bandData.biography}</p>
         </div>
     )
 }
