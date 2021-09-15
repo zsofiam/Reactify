de28ce77e1b20366b9c6cb092273791f74dc7ad0
BrizaMD <late16@gmail.com>
2021-09-15 10:05:43 +0200

    [add] Artist class

---
+++ Reactify/Models/Artist.cs
@@ -0,0 +1,9 @@
+﻿namespace Reactify.Models
+{
+    public class Artist
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Picture { get; set; }
+    }
+}
