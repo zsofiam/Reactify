3796414c210ae3549adf7a54805eb8a2c54c9502
MoniBohm <monic.bohm@gmail.com>
2021-09-16 20:57:33 +0200

    [add] new bg to events page

--- Reactify/ClientApp/src/components/Event/Event.js
+++ Reactify/ClientApp/src/components/Event/Event.js
@@ -4,20 +4,6 @@ const Event = (props) => {
 
     const { id, img, band, genre, date, venue, url } = props.event;
     return (
-
-        // <tr>
-        //     <td className="first-event-column"><img className="event-img" src={img} alt="event" /></td>
-        //     <td>{band}</td>
-        //     <td>
-        //         {genre.map((genreItem, i) => [
-        //             i > 0 && ", ",
-        //             <span key={i}>{genreItem}</span>
-        //         ])}
-        //     </td>
-        //     <td>{date}</td>
-        //     <td>{venue}</td>
-        //     <td><a class="event-link" href={url} target="_blank">Buy Ticket</a></td>
-        // </tr>
        <li class="card">
         <img src={img} alt="event"/>
            <button className="events-button"><span><a target="_blank" href={url}> <i className="fas fa-credit-card"/> Buy ticket</a> </span></button>
