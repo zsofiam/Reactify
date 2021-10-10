a07b0dbe2bee2e4ad299ce7f17ebdb942a19c09e
MoniBohm <monic.bohm@gmail.com>
2021-10-10 14:20:18 +0200

    [refactor] GetResultTracks function

--- Reactify/Controllers/MusicPlayerController.cs
+++ Reactify/Controllers/MusicPlayerController.cs
@@ -22,43 +22,54 @@ namespace Reactify.Controllers
             var url = "https://api.deezer.com/search?q=track:" + track.Trim().Replace(" ", "_");
             using (var httpClient = new HttpClient())
             {
-                httpClient.DefaultRequestHeaders.Add("User-Agent", "Anything");
-                httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-
-                var response = httpClient.GetAsync(url).Result;
-                response.EnsureSuccessStatusCode();
-                var apiResponse = await response.Content.ReadAsStringAsync();
-                var x = JObject.Parse(apiResponse)["data"];
+                JToken albumAndArtist = await GetAlbumAndArtistData(url, httpClient);
                 var result = new List<Track>();
 
-                foreach (var item in x)
+                foreach (var item in albumAndArtist)
                 {
-                    var _track = new Track();
-                    var artist = new Artist();
-                    var album = new Album();
-
-                    _track.Id = (string)item["id"];
-                    _track.Preview = (string)item["preview"];
-                    _track.Title = (string)item["title"];
-                    _track.Duration = (string)item["duration"];
-
-                    artist.Name = (string)item["artist"]["name"];
-                    artist.Id = (string)item["artist"]["id"];
-                    artist.Picture = (string)item["artist"]["picture"];
-
-                    _track.Artist = artist;
-
-                    album.Id = (string)item["album"]["id"];
-                    album.Title = (string)item["album"]["title"];
-                    album.Cover = (string)item["album"]["cover"];
-
-                    _track.Album = album;
-
-                    result.Add(_track);
+                    FillResult(result, item);
                 }
 
                 return result;
             }
+        }
+
+        private async Task<JToken> GetAlbumAndArtistData(string url, HttpClient httpClient)
+        {
+            httpClient.DefaultRequestHeaders.Add("User-Agent", "Anything");
+            httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+            var response = httpClient.GetAsync(url).Result;
+            response.EnsureSuccessStatusCode();
+            var apiResponse = await response.Content.ReadAsStringAsync();
+            var albumAndArtist = JObject.Parse(apiResponse)["data"];
+            return albumAndArtist;
+        }
+
+        private void FillResult(List<Track> result, JToken item)
+        {
+            var _track = new Track();
+            var artist = new Artist();
+            var album = new Album();
+
+            _track.Id = (string)item["id"];
+            _track.Preview = (string)item["preview"];
+            _track.Title = (string)item["title"];
+            _track.Duration = (string)item["duration"];
+
+            artist.Name = (string)item["artist"]["name"];
+            artist.Id = (string)item["artist"]["id"];
+            artist.Picture = (string)item["artist"]["picture"];
+
+            _track.Artist = artist;
+
+            album.Id = (string)item["album"]["id"];
+            album.Title = (string)item["album"]["title"];
+            album.Cover = (string)item["album"]["cover"];
+
+            _track.Album = album;
+
+            result.Add(_track);
         }
     }
 }
