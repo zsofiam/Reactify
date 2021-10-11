a5bcc74c643bf442ad5ae3f054081e2c0e4326a3
MoniBohm <monic.bohm@gmail.com>
2021-10-11 17:32:10 +0200

    [add] to playList return value

--- Reactify/ClientApp/src/components/playList/PlayList.js
+++ Reactify/ClientApp/src/components/playList/PlayList.js
@@ -1,12 +1,43 @@
 ﻿import React from 'react';
-import '../custom.css';
+import '../../custom.css';
 
 
 const PlayList = () => {
 
     return (
+        <div class="music_player">
+            <div class="artist_img">
+                <img src="https://images.macrumors.com/t/vMbr05RQ60tz7V_zS5UEO9SbGR0=/1600x900/smart/article-new/2018/05/apple-music-note.jpg" alt="artist-img" />
+            </div>
+            <div class="time_slider">
+                <span class="runing_time">0:00</span>
+                <input type="range" value="0" />
+                <span class="song_long">0:00</span>
+                <div class="now_playing">
+                    <i class="fa fa-refresh" aria-hidden="true"></i>
 
-        <div className="background-container" data-testid="background-video">
+                    <p> now playing </p>
+                    <i class="fa fa-heart" aria-hidden="true"></i>
+                </div>
+                <div class="music_info">
+                    <h2>adele</h2>
+                    <p class="date">12 - 2016</p>
+                    <p class="song_title">Rolling in the deep</p>
+                </div>
+                <div class="controllers">
+                    <i class="fa fa-music" aria-hidden="true"></i>
+                    <i class="fa fa-fast-backward" aria-hidden="true"></i>
+                    <i class="fa fa-play" aria-hidden="true"></i>
+                    <i class="fa fa-fast-forward" aria-hidden="true"></i>
+                    <i class="fa fa-retweet" aria-hidden="true"></i>
+                </div>
+            </div>
+            <div class="song_list">
+                <div class="title">song name</div>
+                <div class="title">artist name</div>
+                <div class="title dark">song name</div>
+                <div class="title dark">artist name</div>
+            </div>
         </div>
 
     );
