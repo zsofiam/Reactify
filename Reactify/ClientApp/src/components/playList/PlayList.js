bb93703cbfcc0e5febcb321b93ccaf77822e251a
MoniBohm <monic.bohm@gmail.com>
2021-10-11 17:22:43 +0200

    [add] PlayList file

---
+++ Reactify/ClientApp/src/components/playList/PlayList.js
@@ -0,0 +1,16 @@
+﻿import React from 'react';
+import '../custom.css';
+
+
+const PlayList = () => {
+
+    return (
+
+        <div className="background-container" data-testid="background-video">
+        </div>
+
+    );
+
+}
+
+export default PlayList;
