6f00ce79da42b97f0a4a55d8ff876c0e886ea725
MoniBohm <monic.bohm@gmail.com>
2021-10-12 14:02:02 +0200

    [add] PlayerListTest class to project

---
+++ Reactify/ClientApp/src/__test__/PlayerListTest.js
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Reactify.ClientApp.src.__test__
+{
+    public class PlayerListTest
+    {
+    }
+}
