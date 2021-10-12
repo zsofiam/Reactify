edbbdc6cd2e40b0f90de14b55ba5711567b6be10
BrizaMD <late16@gmail.com>
2021-10-12 13:51:47 +0200

    [add] controller for authorization

---
+++ Reactify/Controllers/AuthorizationController.cs
@@ -0,0 +1,20 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Reactify.Controllers
+{
+    
+    [ApiController]
+    [Route("authorization")]
+    public class AuthorizationController : ControllerBase
+    {
+        [HttpPost("register")]
+        public async Task<OkResult> RegisterAccount()
+        {
+            
+            return Ok();
+        }
+
+
+    }
+}
