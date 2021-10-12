b35ab898ac8ea9f40f337a13af91388c050c353e
BrizaMD <late16@gmail.com>
2021-10-12 10:08:40 +0200

    [add] DbContext and AppUser

---
+++ Reactify/Data/ApplicationDbContext.cs
@@ -0,0 +1,24 @@
+﻿using CheckLogin.Models;
+using Reactify.Models;
+using IdentityServer4.EntityFramework.Options;
+using Microsoft.AspNetCore.ApiAuthorization.IdentityServer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
+namespace Reactify.Data
+{
+    public class ApplicationDbContext : ApiAuthorizationDbContext<ApplicationUser>  // this was ApiAuthorizationDbContext
+    {
+        public ApplicationDbContext(
+            DbContextOptions options,
+            IOptions<OperationalStoreOptions> operationalStoreOptions) : base(options, operationalStoreOptions)
+        {
+        }
+
+        //protected override void OnModelCreating(ModelBuilder builder)
+        //{
+        //    base.OnModelCreating(builder);
+        //}
+
+    }
+}
---
+++ Reactify/Models/ApplicationUser.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace CheckLogin.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public string Playlist { get; set; }
+    }
+}
