90fbf619da9553ff45ec4c300c6c7793e0ccb7a5
csilla-hegedus <hegeduscsilla333@gmail.com>
2021-10-12 14:51:11 +0200

    [create] test project for backend testing

---
+++ ReactifyTest/ControllerTests/BandDetailControllerTest.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+
+namespace ReactifyTest.ControllerTests
+{
+    public class BandDetailControllerTest
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
