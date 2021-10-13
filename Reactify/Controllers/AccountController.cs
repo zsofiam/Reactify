c2d608c8af907fe57b1a6775983758eb229db5d5
MoniBohm <monic.bohm@gmail.com>
2021-10-13 17:58:07 +0200

    [add] AccountService and controller

--- Reactify/Controllers/AccountController.cs
+++ Reactify/Controllers/AccountController.cs
@@ -1,11 +1,19 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace Reactify.Controllers
 {
+    [ApiController]
+    [Route("account")]
+
     public class AccountController
     {
+
+        [HttpPost("playlist")]
+        public void GetPlayListByUserId(String userId)
+        { }
     }
 }
---
+++ Reactify/Services/IAccountService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Reactify.Services
+{
+    public interface IAccountService
+    {
+    }
+}
