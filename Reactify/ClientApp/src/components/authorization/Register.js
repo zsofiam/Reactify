97a82524aec56827bd4109ce2837d6750023047f
MoniBohm <monic.bohm@gmail.com>
2021-10-13 13:45:59 +0200

    [fix] redirect after registration

--- Reactify/ClientApp/src/components/authorization/Register.js
+++ Reactify/ClientApp/src/components/authorization/Register.js
@@ -1,35 +1,45 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import './Register.css';
 
 
 export const Register = () => {
 
+    const [isRegistrationDone, setIsRegistrationDone] = useState(false);
+
+    const RegistrationReady = () => {
+        setIsRegistrationDone(true);
+    };
+
     return (
         <div className="registration-container">
-            <form action="/authorization/register" method="POST">
-                <div>
-                    <h1>Register</h1>
-                    <p>Please fill in this form to create an account.</p>
 
-                    <label htmlFor="email"><b>Email</b></label>
-                    <input type="text" placeholder="Enter Email" name="email" id="email" required/>
+            {isRegistrationDone ? <p>Done</p> :
 
-                    <label htmlFor="psw"><b>Password</b></label>
-                    <input type="password" placeholder="Enter Password" name="password" id="psw" required/>
+                <form action="/authorization/register" method="POST">
+                    <div>
+                        <h1>Register</h1>
+                        <p>Please fill in this form to create an account.</p>
 
-                    <label htmlFor="psw-repeat"><b>Repeat Password</b></label>
-                    <input type="password" placeholder="Repeat Password" name="password2" id="psw-repeat"
-                           required/>
+                        <label htmlFor="email"><b>Email</b></label>
+                        <input type="text" placeholder="Enter Email" name="email" id="email" required />
+
+                        <label htmlFor="psw"><b>Password</b></label>
+                        <input type="password" placeholder="Enter Password" name="password" id="psw" required />
+
+                        <label htmlFor="psw-repeat"><b>Repeat Password</b></label>
+                        <input type="password" placeholder="Repeat Password" name="password2" id="psw-repeat"
+                            required />
 
 
-                    <p>By creating an account you agree to our <a href="#">Terms & Privacy</a>.</p>
-                    <button type="submit" className="registerbtn">Register</button>
-                </div>
+                        <p>By creating an account you agree to our <a href="#">Terms & Privacy</a>.</p>
+                        <button type="submit" className="registerbtn" onClick={() => RegistrationReady()}>Register</button>
+                    </div>
 
-                <div className="container-signin">
-                    <p>Already have an account? Use the Login button above! </p>
-                </div>
-            </form>
+                    <div className="container-signin">
+                        <p>Already have an account? Use the Login button above! </p>
+                    </div>
+                </form>
+            }
         </div>
     )
 }
