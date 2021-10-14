509a0b2a09ceacc75b63cc51258f964854ed5d28
BrizaMD <late16@gmail.com>
2021-10-14 13:41:13 +0200

    [add] playlist display

--- Reactify/ClientApp/src/components/playList/PlayList.js
+++ Reactify/ClientApp/src/components/playList/PlayList.js
@@ -33,7 +33,7 @@ const PlayList = () => {
         axios
             .request(options)
             .then(function (response) {
-                console.log(response);
+                console.log(response.data);
                 if (response.data != null) {
                     setFoundUser(true);
                     setPlayList(response.data);
@@ -51,30 +51,37 @@ const PlayList = () => {
                 <MusicPlayer />
             </div>
             <section data-testid="playlist">
-                <div className="album-info">
-                    <div className="album-art"><img src="https://images.unsplash.com/photo-1470225620780-dba8ba36b745?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8MXx8fGVufDB8fHx8&w=1000&q=80"  alt="album cover"/>
-                        <div className="actions">
-                            <div className="play">Play</div>
-                            <div className="bookmark" onClick={() => LikeSong()}>
-                                {isLikedSong ? <i className="fas fa-heart"/> : <i className="far fa-heart"/>}
-                            </div>
-                        </div>
-                    </div>
-                    <div className="album-details">
-                        { /* itt kell majd képet és infókat beadni az actual számról */}
+                {/*<div className="album-info">*/}
+                {/*    <div className="album-art"><img src="https://images.unsplash.com/photo-1470225620780-dba8ba36b745?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8MXx8fGVufDB8fHx8&w=1000&q=80"  alt="album cover"/>*/}
+                {/*        <div className="actions">*/}
+                {/*            <div className="play">Play</div>*/}
+                {/*            <div className="bookmark" onClick={() => LikeSong()}>*/}
+                {/*                {isLikedSong ? <i className="fas fa-heart"/> : <i className="far fa-heart"/>}*/}
+                {/*            </div>*/}
+                {/*        </div>*/}
+                {/*    </div>*/}
+                {/*    /!*<div className="album-details">*!/*/}
+                {/*    /!*    { /* itt kell majd képet és infókat beadni az actual számról *!/*!/*/}
+                {/*    */}
+                {/*    /!*    <h2> <img src="https://68.media.tumblr.com/avatar_edbd71e8c8ac_128.png" />Skillet</h2>*!/*/}
+                {/*    /!*    <h1>Unleashed</h1><span> <span>Hard Rock</span><span>&copy; 2016 Atlantic Recording Corporation</span></span>*!/*/}
+                {/*    /!*    <p>Unleashed is the tenth album by American Christian rock band Skillet, released on August 5, 2016. The album was announced on May 20, 2016, and a lyric video was released for the track "Feel Invincible" at the same time on the band's YouTube channel. Six days later, the band released a lyric video for the track "Stars" on their YouTube channel.</p>*!/*/}
+                {/*    /!*</div>*!/*/}
+                {/*</div>*/}
 
-                        <h2> <img src="https://68.media.tumblr.com/avatar_edbd71e8c8ac_128.png" />Skillet</h2>
-                        <h1>Unleashed</h1><span> <span>Hard Rock</span><span>&copy; 2016 Atlantic Recording Corporation</span></span>
-                        <p>Unleashed is the tenth album by American Christian rock band Skillet, released on August 5, 2016. The album was announced on May 20, 2016, and a lyric video was released for the track "Feel Invincible" at the same time on the band's YouTube channel. Six days later, the band released a lyric video for the track "Stars" on their YouTube channel.</p>
+                { foundUser ?
+                    <div className="album-tracks">
+                        <p> <span>Title</span><span>Artist / Band</span><span><i className="fas fa-clock"/></span></p>
+                        <ol>
+                            {playList.map((track, index) => (
+                                <li key={index}><span>{track.title}</span><span>{track.artistName}</span></li>
+                            ))}
+                        </ol>
                     </div>
-                </div>
-                <div className="album-tracks">
-                    <p> <span>Title</span><span>Artist / Band</span><span><i className="fas fa-clock"/></span></p>
-                    <ol>
-                        <li> <span>Feel Invincible</span><span>Skillet</span><span>3:49</span></li>
-                        <li> <span>Back From The Dead</span><span>3:33</span></li>
-                    </ol>
-                </div>
+                    :
+                    <></>
+                }
+                
             </section>
         </>
 
