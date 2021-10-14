58e565f800e6c15500399cb1b9cf3c20d3eeda52
MoniBohm <monic.bohm@gmail.com>
2021-10-14 17:52:17 +0200

    [reafctor] automatic fromatting

--- Reactify/ClientApp/src/components/musicPlayer/Player.js
+++ Reactify/ClientApp/src/components/musicPlayer/Player.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useRef, useState} from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import PlayerDetails from './PlayerDetails.js';
 import PlayerControls from './PlayerControls';
 import axios from 'axios';
@@ -19,29 +19,35 @@ const Player = (props) => {
     const LikeSong = () => {
         console.log("likeMusic");
         console.log(props.songs[props.currentSongIndex]);
-        
-        if (isLikedSong) setIsLikedSong(false);
+        if (sessionStorage.getItem("userId") === null || sessionStorage.getItem("userId") === "") {
+            return;
+        }
+        const trackAndUserData =  {
+            "UserId": sessionStorage.getItem("userId"),
+                "Id": props.songs[props.currentSongIndex]["id"],
+                    "Title": props.songs[props.currentSongIndex]["title"],
+                        "Duration": props.songs[props.currentSongIndex]["duration"],
+                            "ReleaseDate": props.songs[props.currentSongIndex]["releaseDate"],
+                                "Preview": props.songs[props.currentSongIndex]["preview"],
+                                    "Image": props.songs[props.currentSongIndex]["image"],
+                                        "Artist": props.songs[props.currentSongIndex]["artist"],
+                                            "ArtistName": props.songs[props.currentSongIndex]["artistName"],
+                                                "Album": props.songs[props.currentSongIndex]["album"]
+        }
+
+        if (isLikedSong) {
+            setIsLikedSong(false);
+            axios.delete("tracklist", { data: trackAndUserData }).then(response => console.log("Deleted track", response));
+        }
         else {
             setIsLikedSong(true);
-        };
-     
-
-        axios.post("tracklist", {
-            "Id": props.songs[props.currentSongIndex]["id"],
-            "Title": props.songs[props.currentSongIndex]["title"],
-            "Duration": props.songs[props.currentSongIndex]["duration"],
-            "ReleaseDate": props.songs[props.currentSongIndex]["releaseDate"],
-            "Preview": props.songs[props.currentSongIndex]["preview"],
-            "Image": props.songs[props.currentSongIndex]["image"],
-            "Artist": props.songs[props.currentSongIndex]["artist"],
-            "ArtistName": props.songs[props.currentSongIndex]["artistName"],
-            "Album": props.songs[props.currentSongIndex]["album"]
-        })
-            .then(response => {
-                console.log("Added track to favourites");
-            });
-        
-    };
+            axios.post("tracklist", trackAndUserData)
+                .then(response => {
+                    console.log("Added track to favourites");
+                });
+        }
+        console.log(sessionStorage.getItem("userId"));
+    }
 
 
     const SkipSong = (forwards = true) => {
@@ -73,23 +79,22 @@ const Player = (props) => {
 
     return (
         <div className="c-player">
-            <audio src={props.songs[props.currentSongIndex].preview} ref={audioEl}/>
+            <audio src={props.songs[props.currentSongIndex].preview} ref={audioEl} />
             <h4>Playing now</h4>
-            <PlayerDetails song={props.songs[props.currentSongIndex]}/>
+            <PlayerDetails song={props.songs[props.currentSongIndex]} />
             <div className="heart-icon">
                 <button className="like-btn" onClick={() => LikeSong()}>
                     {isLikedSong ? <i class="fas fa-heart"></i> : <i class="far fa-heart"></i>}
                 </button>
             </div>
             <PlayerControls isPlaying={isPlaying}
-                            setIsPlaying={setIsPlaying}
-                            SkipSong={SkipSong}/>
+                setIsPlaying={setIsPlaying}
+                SkipSong={SkipSong} />
             <p><strong>Next up:</strong>
                 {props.songs[props.nextSongIndex].title}
             </p>
         </div>
     );
-
 }
 
 export default Player;
