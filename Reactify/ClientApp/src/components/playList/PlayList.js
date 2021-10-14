bbe5aa95117502ce4a57c387e010bc0e0e5b2e79
BrizaMD <late16@gmail.com>
2021-10-14 13:42:25 +0200

    [refactor] renamed branch to avoid conflicts

--- Reactify/ClientApp/src/components/playList/PlayList.js
+++ Reactify/ClientApp/src/components/playList/PlayList.js
@@ -60,13 +60,13 @@ const PlayList = () => {
                 {/*            </div>*/}
                 {/*        </div>*/}
                 {/*    </div>*/}
-                {/*    /!*<div className="album-details">*!/*/}
-                {/*    /!*    { /* itt kell majd képet és infókat beadni az actual számról *!/*!/*/}
+                {/*    <div className="album-details">*/}
+                {/*        { /* itt kell majd képet és infókat beadni az actual számról *!/*/}
                 {/*    */}
-                {/*    /!*    <h2> <img src="https://68.media.tumblr.com/avatar_edbd71e8c8ac_128.png" />Skillet</h2>*!/*/}
-                {/*    /!*    <h1>Unleashed</h1><span> <span>Hard Rock</span><span>&copy; 2016 Atlantic Recording Corporation</span></span>*!/*/}
-                {/*    /!*    <p>Unleashed is the tenth album by American Christian rock band Skillet, released on August 5, 2016. The album was announced on May 20, 2016, and a lyric video was released for the track "Feel Invincible" at the same time on the band's YouTube channel. Six days later, the band released a lyric video for the track "Stars" on their YouTube channel.</p>*!/*/}
-                {/*    /!*</div>*!/*/}
+                {/*        <h2> <img src="https://68.media.tumblr.com/avatar_edbd71e8c8ac_128.png" />Skillet</h2>*/}
+                {/*        <h1>Unleashed</h1><span> <span>Hard Rock</span><span>&copy; 2016 Atlantic Recording Corporation</span></span>*/}
+                {/*        <p>Unleashed is the tenth album by American Christian rock band Skillet, released on August 5, 2016. The album was announced on May 20, 2016, and a lyric video was released for the track "Feel Invincible" at the same time on the band's YouTube channel. Six days later, the band released a lyric video for the track "Stars" on their YouTube channel.</p>*/}
+                {/*    </div>*/}
                 {/*</div>*/}
 
                 { foundUser ?
